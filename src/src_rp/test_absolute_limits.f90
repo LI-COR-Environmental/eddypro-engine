1cdc6657c94abedd6cfa501e44bd1391f1204713
Gerardo Fratini <gerardo.fratini@licor.com>
2016-01-08 13:33:02 +0100

    Fix: flag for absolute limits
     * Fix: flags for absolute limits is now correctly calculated. Flags for
       this test obtained so far are unreliable.
    
    Changes to be committed:
    	modified:   src/src_rp/filter_for_physical_thresholds.f90
    	modified:   src/src_rp/test_absolute_limits.f90

--- src/src_rp/test_absolute_limits.f90
+++ src/src_rp/test_absolute_limits.f90
@@ -39,7 +39,6 @@ subroutine TestAbsoluteLimits(Set, N, printout)
     real(kind = dbl), intent(inout) :: Set(N, E2NumVar)
     logical :: printout
     !> local variables
-    integer :: i = 0
     integer :: j = 0
     integer :: hflags(gas4)
     real(kind = dbl) :: HorVel(N)
@@ -47,116 +46,83 @@ subroutine TestAbsoluteLimits(Set, N, printout)
     if (printout) write(*, '(a)', advance = 'no') '   Absolute limits test..'
 
     !> initializations
-    hflags = 9
+    hflags = 0
 
-    !> Checks for values out of bounds in the whole dataset
-    do i = 1, N
-        !>  Wind components
-        HorVel(i) = sqrt((Set(i, u)**2) + (Set(i, v)**2))
-        if (HorVel(i) > al%u_max) then
-            hflags(u) = 1
-            hflags(v) = 1
-            if(RPsetup%filter_al) Set(i, u) = error
-            if(RPsetup%filter_al) Set(i, v) = error
+    !> Flag wind components
+    HorVel(:) = sqrt((Set(:, u)**2) + (Set(:, v)**2))
+    if (any(HorVel > al%u_max)) then
+        hflags(u) = 1
+        hflags(v) = 1
+    end if
+    if (any(abs(Set(:, w)) > al%w_max)) hflags(w) = 1
+    !> Flag sonic temperature
+    if (any(Set(:, ts) < al%t_min + 273.16d0) .or. &
+         any(Set(:, ts) > al%t_max + 273.16d0)) hflags(ts) = 1
+
+    !> Flag co2, expressed as [mmol m-3] if molar_density, [umol mol-1] otherwise
+    if (E2Col(co2)%present) then
+        if(E2Col(co2)%measure_type == 'molar_density') then
+            if (any(Set(:, co2) * StdVair * 1d3 < al%co2_min) &
+                .or. any(Set(:, co2) * StdVair * 1d3 > al%co2_max)) hflags(co2) = 1
         else
-            hflags(u) = 0
-            hflags(v) = 0
+            if (any(Set(:, co2) < al%co2_min) &
+                .or. any(Set(:, co2) > al%co2_max)) hflags(co2) = 1
         end if
-        if(abs(Set(i, w)) > al%w_max) then
-            hflags(w) = 1
-            if(RPsetup%filter_al) Set(i, w) = error
+    end if
+
+    !> Flag h2o, expressed as [mmol m-3] if molar_density, [mmol mol-1] otherwise
+    if (E2Col(h2o)%present) then
+        if(E2Col(h2o)%measure_type == 'molar_density') then
+            if (any(Set(:, h2o) * StdVair * 1d3 < al%h2o_min) &
+                .or. any(Set(:, h2o) * StdVair * 1d3 > al%h2o_max)) hflags(h2o) = 1
         else
-            hflags(w) = 0
+            if (any(Set(:, h2o) < al%h2o_min) &
+                .or. any(Set(:, h2o) > al%h2o_max)) hflags(h2o) = 1
         end if
-        !>  Sonic temperature
-        if ((Set(i, ts) < al%t_min + 273.16d0) .or. &
-            (Set(i, ts) > al%t_max + 273.16d0) ) then
-            hflags(ts) = 1
-            if(RPsetup%filter_al) Set(i, ts) = error
+    end if
+
+    !> Flag ch4, expressed as [mmol m-3] if molar_density, [umol mol-1] otherwise
+    if (E2Col(ch4)%present) then
+        if(E2Col(ch4)%measure_type == 'molar_density') then
+            if (any(Set(:, ch4) * StdVair * 1d3 < al%ch4_min) &
+                .or. any(Set(:, ch4) * StdVair * 1d3 > al%ch4_max)) hflags(ch4) = 1
         else
-            hflags(ts) = 0
+            if (any(Set(:, ch4) < al%ch4_min) &
+                .or. any(Set(:, ch4) > al%ch4_max)) hflags(ch4) = 1
         end if
+    end if
 
-        !> co2, expressed as [mmol m-3] if molar_density, [umol mol-1] otherwise
-        if (E2Col(co2)%present) then
-            if(E2Col(co2)%measure_type == 'molar_density') then
-                if( (Set(i, co2) * StdVair * 1d3 < al%co2_min) .or. &
-                    (Set(i, co2) * StdVair * 1d3 > al%co2_max) ) then
-                    hflags(co2) = 1
-                else
-                    hflags(co2) = 0
-                end if
-            else
-                if( (Set(i, co2) < al%co2_min) .or. &
-                    (Set(i, co2) > al%co2_max) ) then
-                    hflags(co2) = 1
-                else
-                    hflags(co2) = 0
-                end if
-            end if
+    !> Flag gas4, expressed as [mmol m-3] if molar_density, [umol mol-1] otherwise
+    if (E2Col(gas4)%present) then
+        if(E2Col(gas4)%measure_type == 'molar_density') then
+            if (any(Set(:, gas4) * StdVair * 1d3 < al%gas4_min) &
+                .or. any(Set(:, gas4) * StdVair * 1d3 > al%gas4_max)) hflags(gas4) = 1
+        else
+            if (any(Set(:, gas4) < al%gas4_min) &
+                .or. any(Set(:, gas4) > al%gas4_max)) hflags(gas4) = 1
         end if
+    end if
 
-        !> h2o, expressed as [mmol m-3] if molar_density, [mmol mol-1] otherwise
-        if (E2Col(h2o)%present) then
-            if(E2Col(h2o)%measure_type == 'molar_density') then
-                if( (Set(i, h2o) * StdVair < al%h2o_min) .or. &
-                    (Set(i, h2o) * StdVair > al%h2o_max) ) then
-                    hflags(h2o) = 1
-                else
-                    hflags(h2o) = 0
-                end if
-            else
-                if( (Set(i, h2o) < al%h2o_min) .or. &
-                    (Set(i, h2o) > al%h2o_max) ) then
-                    hflags(h2o) = 1
-                else
-                    hflags(h2o) = 0
-                end if
-            end if
-        end if
-
-        !> ch4, expressed as [mmol m-3] if molar_density, [umol mol-1] otherwise
-        if (E2Col(ch4)%present) then
-            if(E2Col(ch4)%measure_type == 'molar_density') then
-                if( (Set(i, ch4) * StdVair * 1d3 < al%ch4_min) .or. &
-                    (Set(i, ch4) * StdVair * 1d3 > al%ch4_max) ) then
-                    hflags(ch4) = 1
-                else
-                    hflags(ch4) = 0
-                end if
-            else
-                if( (Set(i, ch4) < al%ch4_min) .or. &
-                    (Set(i, ch4) > al%ch4_max) ) then
-                    hflags(ch4) = 1
-                else
-                    hflags(ch4) = 0
-                end if
-            end if
-        end if
-
-        !> gas4, expressed as [mmol m-3] if molar_density, [umol mol-1] otherwise
-        if (E2Col(gas4)%present) then
-            if(E2Col(gas4)%measure_type == 'molar_density') then
-                if( (Set(i, gas4) * StdVair * 1d3 < al%gas4_min) .or. &
-                    (Set(i, gas4) * StdVair * 1d3 > al%gas4_max) ) then
-                    hflags(gas4) = 1
-                else
-                    hflags(gas4) = 0
-                end if
-            else
-                if( (Set(i, gas4) < al%gas4_min) .or. &
-                    (Set(i, gas4) > al%gas4_max) ) then
-                    hflags(gas4) = 1
-                else
-                    hflags(gas4) = 0
-                end if
-            end if
-        end if
-    end do
     !> Create an 8-digits number containing the values of the hflags
     IntHF%al = 900000000
     do j = 1, gas4
         IntHF%al = IntHF%al + hflags(j) * (10**(gas4 - j))
     end do
-     if (printout) write(*,'(a)') ' Done.'
+
+    !> Filter sonic measurements for absolute limits
+    if(RPsetup%filter_al) then
+        where (HorVel > al%u_max)
+            Set(:, u) = error
+            Set(:, v) = error
+        end where
+        where (abs(Set(:, w)) > al%w_max)
+            Set(:, w) = error
+        end where
+        where ( Set(:, ts) < al%t_min + 273.16d0 .or. &
+            Set(:, ts) > al%t_max + 273.16d0)
+            Set(:, ts) = error
+        end where
+    end if
+
+    if (printout) write(*,'(a)') ' Done.'
 end subroutine TestAbsoluteLimits
