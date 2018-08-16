a6f0739f9dbb1845838b0fa7bddce31c605e11a9
Gerardo Fratini <gerardof@gerardo-fs-mbp.speedport_w_921v_1_44_000>
2018-08-16 15:00:05 +0200

    Change label fluxnet_mode to fluxnet_err_label

--- src/src_common/m_common_global_var.f90
+++ src/src_common/m_common_global_var.f90
@@ -478,7 +478,7 @@ module m_common_global_var
          EPPrjCTags(46)%Label / 'hf_correct_ghg_ba'  / &
          EPPrjCTags(47)%Label / 'hf_correct_ghg_zoh' / &
          EPPrjCTags(48)%Label / 'fluxnet_standardize_biomet' / & 
-         EPPrjCTags(49)%Label / 'fluxnet_mode' / 
+         EPPrjCTags(49)%Label / 'fluxnet_err_label' / 
 
     !> tags of the metadata file created by GHG software
     integer, parameter :: Nan = 884
