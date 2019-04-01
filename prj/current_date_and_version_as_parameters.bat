4bcb0da8d6aa2cba65102b70ed005d5ffc01098b
Gerardo Fratini <gerardo.fratini@licor.com>
2019-04-01 14:36:33 +0200

    Update script to bump version and build date to work on shell

--- prj/current_date_and_version_as_parameters.bat
+++
@@ -1,6 +0,0 @@
-set datetime=%date%, %time%
-set datetime=%datetime:~0,17%
-echo character(5), parameter  :: sw_ver = '6.1.0' > ..\src\src_common\version_and_date.inc
-echo character(17), parameter :: build_date = '%datetime%' >> ..\src\src_common\version_and_date.inc
-cd ..\src\src_common\
-copy /b init_env.f90 +,, > nul
